dbe92b2e2028c8d110ef7354e43d0b18249cf7e7
gitlost-murali <kmanoharmurali@gmail.com>
2019-10-17 20:00:02 +0530

    Cleaned code

--- ocr_tesseract/ocrpy.py
+++ ocr_tesseract/ocrpy.py
@@ -1,12 +1,7 @@
 import pytesseract
 from PIL import Image, ImageEnhance, ImageFilter
 
-# im = Image.open("test.png") # the second one
-# im = im.filter(ImageFilter.MedianFilter())
-# enhancer = ImageEnhance.Contrast(im)
-# im = enhancer.enhance(2)
-# im = im.convert('1')
-# im.save('temp2.jpg')
-pytesseract.pytesseract.tesseract_cmd ='D:/Program Files/Tesseract-OCR/tesseract.exe' 
+
+pytesseract.pytesseract.tesseract_cmd ='D:/Program Files/Tesseract-OCR/tesseract.exe'
 text = pytesseract.image_to_string(Image.open('test.png'))
 print(text)
